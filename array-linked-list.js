25e3093e21c3e2dc000fc39d8c0c136fc80789c5
SR Sajjad <sajjadsr001@gmail.com>
2020-05-02 12:23:24 +0600

    Create array-linked-list.js

---
+++ array-linked-list.js
@@ -0,0 +1,9 @@
+export const arrayToLinkedList = (arr, i = 0) => {
+  let val = arr[i];
+  let next = isFinite(arr[i + 1]) ? arrayToLinkedList(arr, i + 1) : null;
+
+  return { val, next };
+};
+
+
+// todo: add linked list to array
