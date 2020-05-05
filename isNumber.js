2d3236a96fd51d0e18d163e9f38c5f713560d659
SR Sajjad <sajjadsr001@gmail.com>
2020-05-05 21:19:36 +0600

    prettify

--- isNumber.js
+++ isNumber.js
@@ -1,4 +1,4 @@
-export function isNumber(x) {
-  if((x > 0 || x < 0 || x === 0) && typeof x === 'number') return true
-  else return false
+function isNumber(x) {
+  if ((x > 0 || x < 0 || x === 0) && typeof x === "number") return true;
+  else return false;
 }
