6a6ea31f37fec784e90a1fa4f50bc2adde342044
SR Sajjad <sajjadsr001@gmail.com>
2020-05-05 21:19:06 +0600

    Update mergeTwoSortedArray.js
    resolve boundary case - Infinity

--- mergeTwoSortedArray.js
+++ mergeTwoSortedArray.js
@@ -1,16 +1,23 @@
-export const merge = (a = [], b = []) => {
+export const merge = (l = [], r = []) => {
   let arr = [];
-
-  let l = [...a, Infinity];
-  let r = [...b, Infinity];
 
   let i = 0;
   let j = 0;
 
-  while (i < l.length - 1 || j < r.length - 1) {
-    if (l[i] <= r[j]) arr.push(l[i++]);
-    else if (r[j] <= l[i]) arr.push(r[j++]);
+  while (i < l.length || j < r.length) {
+    if (isNumber(l[i]) && isNumber(r[j])) {
+      if (l[i] <= r[j]) arr.push(l[i++]);
+      if (r[j] <= l[i]) arr.push(r[j++]);
+    }
+    
+    else if (isNumber(l[i])) arr.push(l[i++]);
+    else if (isNumber(r[j])) arr.push(r[j++]);
   }
 
   return arr;
 };
+
+function isNumber(x) {
+  if ((x > 0 || x < 0 || x === 0) && typeof x === "number") return true;
+  else return false;
+}
